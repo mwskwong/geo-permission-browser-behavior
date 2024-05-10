482823b1b9760c2f89a327a373f13e64cbfb3081
Matthew Kwong <matthew.ws.kwong@hkjc.org.hk>
2024-05-10 13:02:55 +0800

    refactor: minor condition change

--- src/App.tsx
+++ src/App.tsx
@@ -29,33 +29,33 @@ export default function App() {
       <p>{geolocation?.status}</p>
       <pre style={{ textAlign: "start" }}>
         <p>
-          {geolocation?.status === "success"
-            ? JSON.stringify(
-                {
-                  coords: {
-                    latitude: geolocation.position.coords.latitude,
-                    longitude: geolocation.position.coords.longitude,
-                    altitude: geolocation.position.coords.altitude,
-                    accuracy: geolocation.position.coords.accuracy,
-                    altitudeAccuracy:
-                      geolocation.position.coords.altitudeAccuracy,
-                    heading: geolocation.position.coords.heading,
-                    speed: geolocation.position.coords.speed,
+          {geolocation &&
+            (geolocation.status === "success"
+              ? JSON.stringify(
+                  {
+                    coords: {
+                      latitude: geolocation.position.coords.latitude,
+                      longitude: geolocation.position.coords.longitude,
+                      altitude: geolocation.position.coords.altitude,
+                      accuracy: geolocation.position.coords.accuracy,
+                      altitudeAccuracy:
+                        geolocation.position.coords.altitudeAccuracy,
+                      heading: geolocation.position.coords.heading,
+                      speed: geolocation.position.coords.speed,
+                    },
+                    timestamp: geolocation.position.timestamp,
                   },
-                  timestamp: geolocation.position.timestamp,
-                },
-                null,
-                2
-              )
-            : geolocation?.error &&
-              JSON.stringify(
-                {
-                  code: geolocation.error.code,
-                  message: geolocation.error.message,
-                },
-                null,
-                2
-              )}
+                  null,
+                  2
+                )
+              : JSON.stringify(
+                  {
+                    code: geolocation.error.code,
+                    message: geolocation.error.message,
+                  },
+                  null,
+                  2
+                ))}
         </p>
       </pre>
     </div>
