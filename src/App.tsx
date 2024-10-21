8956fbee0ecaf23da69e0d0a24b67ad903b6b2e5
Matthew Kwong <32725014+mwskwong@users.noreply.github.com>
2024-10-21 18:58:44 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -65,54 +65,6 @@ export default function App() {
       >
         call and await API before setting state to trigger useEffect to call window.open
       </button>
-      <button
-        onClick={() => {
-          setGeolocation({ status: "loading" });
-          navigator.geolocation.getCurrentPosition(
-            (position) => setGeolocation({ status: "success", position }),
-            (error) => setGeolocation({ status: "error", error }),
-            { enableHighAccuracy: true }
-          );
-        }}
-      >
-        Request Geo-location
-      </button>
-      <p>{geolocation?.status}</p>
-      <pre style={{ textAlign: "start" }}>
-        <p>
-          {(() => {
-            switch (geolocation?.status) {
-              case "success":
-                return JSON.stringify(
-                  {
-                    coords: {
-                      latitude: geolocation.position.coords.latitude,
-                      longitude: geolocation.position.coords.longitude,
-                      altitude: geolocation.position.coords.altitude,
-                      accuracy: geolocation.position.coords.accuracy,
-                      altitudeAccuracy:
-                        geolocation.position.coords.altitudeAccuracy,
-                      heading: geolocation.position.coords.heading,
-                      speed: geolocation.position.coords.speed,
-                    },
-                    timestamp: geolocation.position.timestamp,
-                  },
-                  null,
-                  2
-                );
-              case "error":
-                return JSON.stringify(
-                  {
-                    code: geolocation.error.code,
-                    message: geolocation.error.message,
-                  },
-                  null,
-                  2
-                );
-            }
-          })()}
-        </p>
-      </pre>
     </div>
   );
 }
