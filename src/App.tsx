0d196cf2605d4361a9eef2cf02b80293db34325a
Matthew Kwong <32725014+mwskwong@users.noreply.github.com>
2024-11-25 16:48:15 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-export default function App() {
+import { useRef } from 'react';
 
+export default function App() {
+  const divRef = useRef(null);
+  
   return (
-    <div>
+    <div
+      ref={divRef}
+      onClick={(target) => {
+        console.log({
+          target: event.target,
+          ref: divRef.current,
+          isEqual: divRef.current === event.target
+        })
+      }
+    >
       <h1>Heading 1</h1>
       <p 
         style={{
