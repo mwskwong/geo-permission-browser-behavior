fe29313550670ff2013e3eed5e7fcd493d384610
Matthew Kwong <32725014+mwskwong@users.noreply.github.com>
2024-11-27 10:43:38 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,23 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
-export default function App() {
-  const divRef = useRef(null);
+export default function App() {  
+  const [canNext, setCanNext] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCanNext(true);
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [])
   
   return (
-    <div
-      ref={divRef}
-      onClick={(event) => {
-        console.log({
-          target: event.target,
-          ref: divRef.current,
-          isEqual: divRef.current === event.target
-        })
-      }}
-    >
-      <h1>Heading 1</h1>
-      <p 
-        style={{
-          boxSizing: "border-box",
-          fontSize: "13px",
-          textSizeAdjust: "100%",
-        }}
-        onClick={() => console.log("yo")}
-      >
-        Paragraph
-      </p>
-      <button
-        onClick={() => {
-          sleep(3000).finally(() => window.close());
-          window.open("https://google.com");
-        }}  
-      >
-        Button
-      </button>
+    <div>
+      <div className={`startBtn ${canNext ? '' : 'disable'}`}>
+        some text
+      </div>
     </div>
   );
 }
