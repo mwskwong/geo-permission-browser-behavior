4b6700103323bd63bb77edfa38e37448e0aa1874
Matthew Kwong <32725014+mwskwong@users.noreply.github.com>
2024-11-27 14:59:37 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-const url = "https://sim.fps.payapps.hkicl.com.hk/?pay_req_obj=https%3A%2F%2Fexample.com&callback=https%3A%2F%2Fexample.com";
+const url = "https://fps.payapps.hkicl.com.hk/?pay_req_obj=https%3A%2F%2Fexample.com&callback=https%3A%2F%2Fexample.com";
 
 export default function App() {  
   return (
