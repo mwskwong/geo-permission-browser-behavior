a09b61ff2596d8b4bbb2c4e0e3cd9d98f4ac9704
Matthew Kwong <32725014+mwskwong@users.noreply.github.com>
2025-02-07 17:34:33 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
-
 export default function App() {
-  const hkiclUrl = "https://fps.payapps.hkicl.com.hk/";
   return (
     <div style={{ height: 2000 }}>
-
-      <div style={{ position: "fixed", bottom: 0, right: 0, width: "100%", padding: 16, backgroundColor: "red" }} className="bottomBar">
+      <div
+        style={{
+          position: "fixed",
+          bottom: 0,
+          right: 0,
+          width: "100%",
+          padding: 16,
+          backgroundColor: "red",
+        }}
+        className="bottomBar"
+      >
         stick to bottom
       </div>
     </div>
