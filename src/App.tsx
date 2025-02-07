6fa83f811ab2831ad013be3c658cb2b3c6e4aff4
Matthew Kwong <32725014+mwskwong@users.noreply.github.com>
2025-02-07 17:32:38 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div style={{ height: 2000 }}>
 
-      <div style={{ position: "fixed", bottom: 0, right: 0, width: "100%", padding: 16, backgroundColor: "red" }}>
+      <div style={{ position: "fixed", bottom: 0, right: 0, width: "100%", padding: 16, backgroundColor: "red" }} className="bottomBar">
         stick to bottom
       </div>
     </div>
