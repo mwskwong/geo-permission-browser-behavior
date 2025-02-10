8ac74c7366d1a3e4cfddea14e5ab9013d9f1b7b0
Matthew Kwong <matthew.ws.kwong@hkjc.org.hk>
2025-02-10 13:42:22 +0800

    feat: img display block

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ export default function App() {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua.
         <br />
+        <br />
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
         ut aliquip ex ea commodo consequat.
       </span>
