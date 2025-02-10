db2456750a354d7329b54dd53df822bdf9ba387a
Matthew Kwong <matthew.ws.kwong@hkjc.org.hk>
2025-02-10 13:27:43 +0800

    feat: remove wrapping div

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import styles from "./App.module.css";
 
 export default function App() {
   return (
-    <div>
-      <img className={styles.loading} src={loading} width={40} height={41} />
-    </div>
+    <img className={styles.loading} src={loading} width={40} height={41} />
   );
 }
